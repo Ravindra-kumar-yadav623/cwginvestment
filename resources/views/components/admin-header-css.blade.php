13e1a599bceccabb5ca5301bfc518fc07b7d9f23
Ravindra-kumar-yadav623 <ravindrachitarpur@gmail.com>
2025-12-07 11:42:21 +0530

    Some changes

--- resources/views/components/admin-header-css.blade.php
+++ resources/views/components/admin-header-css.blade.php
@@ -1,46 +1,32 @@
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>Capital Wealth Growth</title>
-        <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
-        <link rel="icon" href="{{asset('assets/img/kaiadmin/favicon.ico')}}" type="image/x-icon" />
-        <!-- Fonts and icons -->
-        <script src="{{asset('assets/js/plugin/webfont/webfont.min.js')}}"></script>
-        <script>
-          WebFont.load({
-            google: {
-              families: ["Public Sans:300,400,500,600,700"]
-            },
-            custom: {
-              families: ["Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons", ],
-              urls: ["{{asset('assets/css/fonts.min.css')}}"],
-            },
-            active: function() {
-              sessionStorage.fonts = true;
-            },
-          });
-        </script>
-        <!-- CSS Files -->
-        <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" />
-        <link rel="stylesheet" href="{{asset('assets/css/plugins.min.css')}}" />
-        <link rel="stylesheet" href="{{asset('assets/css/kaiadmin.min.css')}}" />
-        <!-- CSS Just for demo purpose, don't include it in your project -->
-        <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}" />
-
-
-        
-        <!-- google fonts -->
-        <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
-
-         <!-- files -->
-        <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
-        <link href="{{asset('assets/css/magnific-popup.css')}}" rel="stylesheet">
-        <link href="{{asset('assets/css/owl.carousel.css')}}" rel="stylesheet">
-        <link href="{{asset('assets/css/owl.carousel.theme.min.css')}}" rel="stylesheet">
-        <link href="{{asset('assets/css/ionicons.css')}}" rel="stylesheet">
-        <link href="{{asset('assets/css/main.css')}}" rel="stylesheet">
-
-
-      </head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    {{asset('assets/css/main.css')}}
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Capital Wealth Growth</title>
+    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
+    <link rel="icon" href="{{asset('assets/img/kaiadmin/favicon.ico')}}" type="image/x-icon" />
+    <!-- Fonts and icons -->
+    <script src="{{asset('assets/js/plugin/webfont/webfont.min.js')}}"></script>
+    <script>
+      WebFont.load({
+        google: {
+          families: ["Public Sans:300,400,500,600,700"]
+        },
+        custom: {
+          families: ["Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons", ],
+          urls: ["{{asset('assets/css/fonts.min.css')}}"],
+        },
+        active: function() {
+          sessionStorage.fonts = true;
+        },
+      });
+    </script>
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="{{asset('assets/css/admin/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/plugins.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/kaiadmin.min.css')}}" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}" />
+  </head>
+      
